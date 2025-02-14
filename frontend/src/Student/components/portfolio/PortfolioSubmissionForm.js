982a7ed1b00538fb435b39e6eb8974da9c92f6ee
Masonk31 <mgk7525@rit.edu>
2025-02-13 19:21:39 -0500

    Removed test comments from debugging

--- frontend/src/Student/components/portfolio/PortfolioSubmissionForm.js
+++ frontend/src/Student/components/portfolio/PortfolioSubmissionForm.js
@@ -213,7 +213,7 @@ function PortfolioSubmissionForm(props) {
             <SubHeader>Upload Media Submissions</SubHeader>
             <b>You may upload up to 10 submissions. (Photo, Video, or Other Media)</b>
             <p>You may only upload one file OR video link per row.</p>
-            {/* {form_data.submissions.length > 0 &&
+            {form_data.submissions.length > 0 &&
               form_data.submissions.map((submission, index) => {
                 return (
                   <div key={`form_data.submissions.${submission.id}`}>
@@ -232,7 +232,7 @@ function PortfolioSubmissionForm(props) {
                     <hr />
                   </div>
                 )
-              })} */}
+              })}
 
             <FormGroup>
               <WorkReleasePopover />
