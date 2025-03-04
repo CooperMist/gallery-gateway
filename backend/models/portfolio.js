37bc2d8201426bd5048c3f8545fc62db6a536ff3
Cooper <coopmist42@gmail.com>
2025-03-04 18:47:16 -0500

    WIP backend changes

--- backend/models/portfolio.js
+++ backend/models/portfolio.js
@@ -1,6 +1,7 @@
 import DataTypes from 'sequelize'
 import sequelize from '../config/sequelize'
 import Entry from './entry'
+import PortfolioRating from './portfolioRating'
 
 // Defines a portfolio object and all of its fields
 const Portfolio = sequelize.define('portfolio', {
@@ -35,11 +36,32 @@ const Portfolio = sequelize.define('portfolio', {
     },
     onDelete: 'no action',
     onUpdate: 'cascade'
+  },
+  score: {// added score to support scholarship judging functionality
+    type: DataTypes.DOUBLE,
+    defaultValue: 0,
+    allowNull: false
   }
 })
 
 Portfolio.prototype.getEntries = function getEntries () {
   return Entry.findAll({ where: { portfolioId: this.id } })
+}
+
+/*
+* Calculate the score for a portfolio
+*/
+PortfolioRating.prototype.getScore = function getScore () {
+  // Calculate score by getting all votes with this
+  // entry id and then averaging over the sum of the votes
+  return PortfolioRating.findAll({ where: { porfolioId: this.id } })
+    .then((ratings) => {
+      const ratingValues = ratings.map(rating => rating.value)
+      if (ratingValues.length === 0) {
+        return 0
+      }
+      return ratingValues.reduce((acc, curr) => acc + curr) / ratingValues.length
+    })
 }
 
 export default Portfolio
---
+++ backend/models/portfolioRating.js
@@ -0,0 +1,55 @@
+import User from './user'
+import DataTypes from 'sequelize'
+import sequelize from '../config/sequelize'
+import { IMAGE_ENTRY, VIDEO_ENTRY, OTHER_ENTRY } from '../constants'
+import { ALLOWED_RATING_VALUES } from '../constants'
+
+const PortfolioRating = sequelize.define('portfolioRating', {
+  // id of portfolio submitted
+  portfolioId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'portfolios',
+      key: 'id'
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade'
+  },
+  value: {
+    type: DataTypes.DOUBLE,
+    defaultValue: 0,
+    allowNull: false
+  },
+    // The username of the judge for the Portfolio Period
+    judgeUsername: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+        model: 'users',
+        key: 'username'
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade'
+},
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE
+  },
+  updatedAt: {
+    allowNull: false,
+    type: DataTypes.DATE
+  }
+},
+{
+    validate: {
+        ratingValueValidation () {
+          if (!ALLOWED_RATING_VALUES.has(this.value)) {
+            throw new Error('Rating value must be an integer between 1 and 5 inclusive.')
+          }
+        }
+      }
+})
+
+
+export default PortfolioRating
